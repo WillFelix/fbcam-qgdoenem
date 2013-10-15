948d1f72ffc1facb923e316b1ba800ef1d13f187
Will Felix <willfelix93@gmail.com>
2013-10-15 16:09:37 -0300

    App Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbcamQgdoenem::Application.config.secret_token = '46956d764efeca4a3510ec0533e030fdba6a055b29d73d2f5bbee2b5b0699db834679b6690d6e712dab975c2dee44b1d65372d00d650908685d55e7fb24d5622'
